aa8cf7eb1de215b109964b29d68ef087ff7ad38e
PawanGaria <pawangaria@MacBookAir.lan>
2026-09-22 13:55:22 +0530

    Fix database connection setup

--- inventory-backend/app/database.py
+++ inventory-backend/app/database.py
@@ -1,18 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://pawangaria@localhost:5432/inventory_db"
 
-engine = create_engine(
-    DATABASE_URL,
-    echo=False,
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://pawangaria@localhost:5432/inventory_db"
 )
+
+
+engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine,
 )
+
 
 Base = declarative_base()
 
